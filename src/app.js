fe4dbdda206cb0d5f9e960bff3b6b3bd654fdc2e
ziayiing <ziayiing86@gmail.com>
2023-07-02 14:32:35 +0800

    updated apikey

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "94f07b95db0a2986f8f13bba8b6ab533";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
